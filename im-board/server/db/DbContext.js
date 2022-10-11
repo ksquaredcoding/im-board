fb223cf3e7c7ce949297e1bffd90d4bf44817dc7
Kevin Kelley <ksquaredcoding@gmail.com>
2022-10-11 14:08:46 -0600

    BoardGame Schema and BoardGames collection

--- im-board/server/db/DbContext.js
+++ im-board/server/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { BoardGameSchema } from "../models/boardgame.js";
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  BoardGame = mongoose.model('BoardGame', BoardGameSchema);
 }
 
 export const dbContext = new DbContext()
---
+++ im-board/server/models/BoardGame.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import { ObjectId, SCHEMA_OPTIONS } from "../db/DbUtils.js"
+const Schema = mongoose.Schema
+
+export const BoardGameSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    gameId: { type: String, required: true },
+    type: { type: String, required: true, enum: ['owned', 'wish', 'favorite'] },
+    imgUrl: { type: String, required: true },
+    rating: { type: Number, required: true, min: 0, max: 5 },
+    accountId: { type: ObjectId, ref: 'Account', required: true }
+  }, SCHEMA_OPTIONS)
+
+BoardGameSchema.virtual('account', {
+  localField: 'accountId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Profile'
+})
