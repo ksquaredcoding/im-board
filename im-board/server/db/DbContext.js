f7a5ba031439e873bcd2c6dd52f82fb01ecff659
Kevin Kelley <ksquaredcoding@gmail.com>
2022-10-11 14:33:41 -0600

    boardgames service

--- im-board/server/db/DbContext.js
+++ im-board/server/db/DbContext.js
@@ -6,7 +6,7 @@ import { ValueSchema } from '../models/Value'
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
-  BoardGame = mongoose.model('BoardGame', BoardGameSchema);
+  BoardGames = mongoose.model('BoardGame', BoardGameSchema);
 }
 
 export const dbContext = new DbContext()
--- im-board/server/services/BoardGamesService.js
+++ im-board/server/services/BoardGamesService.js
@@ -2,13 +2,13 @@ import { dbContext } from '../db/DbContext'
 import { BadRequest } from '../utils/Errors'
 
 class BoardGamesService {
-  async find(query = {}) {
-    const values = await dbContext.Values.find(query)
-    return values
+  async find(accountId) {
+    const games = await dbContext.BoardGames.find(g => accountId == g.accountId)
+    return games
   }
 
   async findById(id) {
-    const value = await dbContext.Values.findById(id)
+    const value = await dbContext.BoardGames.findById(id)
     if (!value) {
       throw new BadRequest('Invalid Id')
     }
