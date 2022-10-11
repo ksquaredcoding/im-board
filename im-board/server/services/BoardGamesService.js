73f6665796e7124967ba1bdb84fae3f569adeaac
Kevin Kelley <ksquaredcoding@gmail.com>
2022-10-11 14:54:15 -0600

    add and delete game in service -kk

--- im-board/server/services/BoardGamesService.js
+++ im-board/server/services/BoardGamesService.js
@@ -7,12 +7,24 @@ class BoardGamesService {
     return games
   }
 
-  async findById(id) {
-    const value = await dbContext.BoardGames.findById(id)
-    if (!value) {
-      throw new BadRequest('Invalid Id')
+  async getBoardGameById(id) {
+    const boardGame = await dbContext.BoardGames.findById(id)
+    if (!boardGame) {
+      throw new BadRequest('Invalid BoardGame Id')
     }
-    return value
+    return boardGame
+  }
+
+  async addBoardGameToList(boardGameData) {
+    const game = await dbContext.BoardGames.create(boardGameData)
+    await game.populate('Account', 'name picture')
+    return game
+  }
+
+  async deleteBoardGameFromList(boardGameId) {
+    const game = await dbContext.BoardGames.findById(boardGameId)
+    await game.remove()
+    return game
   }
 }
 
