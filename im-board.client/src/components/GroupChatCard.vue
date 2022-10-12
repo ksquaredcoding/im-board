bc1d6fdb84e487fed0e9d1676582a0b0002bd202
TungLe0319 <tung.le0319@gmail.com>
2022-10-12 15:47:12 -0600

    tung

---
+++ im-board.client/src/components/GroupChatCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="groupChatCard card bg-c2 my-2 elevation-5" >
+    <div class="d-flex p-1 align-items-end justify-content-around">
+      <img src="//thiscatdoesnotexist.com" alt="" height="45" width="45" class="rounded-circle box-shadow profile-img">
+      <p class="ms-1">Member Name</p>
+    </div>
+    <div class="bg-dark m-1 rounded ps-2 py-1">
+Wow, this group feature rocks!When are we playing next?
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+  },
+  setup(props) {
+    return {
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.text-shadow{
+  color: aliceblue;
+  text-shadow: 1px 1px black, 0px 0px 5px salmon;
+  font-weight: bold;
+  letter-spacing: 0.08rem
+  /* Second Color  in text-shadow is the blur */
+}
+.forcedImg{
+  height: 300px;
+  width: 300px;
+  object-fit: cover;
+}
+</style>
