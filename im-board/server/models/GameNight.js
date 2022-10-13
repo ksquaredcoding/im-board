34653ee156cf8bdb96adf53baef0b27955eb4140
daniel le <daniel97.le@gmail.com>
2022-10-13 15:06:14 -0600

    commit

--- im-board/server/models/GameNight.js
+++ im-board/server/models/GameNight.js
@@ -6,8 +6,10 @@ export const GameNightSchema = new Schema(
   {
     groupId: { type: ObjectId, ref: "Group", required: true },
     location: { type: String, required: true, minlength: 1, maxlength: 50 },
-    gameId: { type: ObjectId, required: true, ref: "Game" },
-    groupMemberIds: [{ type: ObjectId, ref: "Account", required: false }],
+    gameId: { type: String, required: true, ref: "Game" },
+    groupMemberIdsAttending: [
+      { type: ObjectId, ref: "Account", required: false },
+    ],
     startDate: { type: Date, required: true },
   },
   SCHEMA_OPTIONS
