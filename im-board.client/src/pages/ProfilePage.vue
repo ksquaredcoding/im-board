abe34da015519edc5537df997184c9096b24f746
molly-nettleton <mollyelainen@gmail.com>
2022-10-13 14:36:41 -0600

    Profile Page Complete - Molly

--- im-board.client/src/pages/ProfilePage.vue
+++ im-board.client/src/pages/ProfilePage.vue
@@ -1,34 +1,91 @@
 <template>
   <div class="container-fluid">
+
   <div class="row bg-c5 banner eum-ipsum">
-        <div class="col-md-12 d-flex justify-content-center ">
- <img src="//thiscatdoesnotexist.com" alt="" height="150" width="150" class="eum rounded-circle mt-2 icon"></div></div>
-    </div>
+  <div class="col-md-12 d-flex justify-content-center ">
+  <img src="//thiscatdoesnotexist.com" alt="" height="150" width="150" class="eum rounded-circle mt-2 icon">
+  </div>
+  </div>
 
   <div class="row justify-content-center align-items-center p-4">
   </div>
-  <div class="row justify-content-center align-items-center g-2 mt-3">
+  <div class="row justify-content-center align-items-center g-2 mt-3 mb-2">
     <div class="col-md-12 d-flex justify-content-center username">User Name</div>
-    
-    <div class="row justify-content-center align-items-center g-2">
-      <div class="col-md-12 bg-dark cardholder">Column</div>
     </div>
+    
+    <div class="row bg-dark flex-wrap justify-content-between pt-4 pb-3">
+      
+        <div class="col mx-3 bg-grey">
+          <div class="row bg-c4">
+            <div class="col d-flex justify-content-center pt-2"><h2>Favorites</h2></div>
+            </div>
+            <div class="cardholder">
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+            </div>
+        </div>
+
+         <div class="col mx-3 bg-grey">
+          <div class="row bg-c4">
+            <div class="col d-flex justify-content-center pt-2"><h2>Games I Own</h2></div>
+            </div>
+            <div class="cardholder">
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+               <GroupGamesCard/>
+                <GroupGamesCard/>
+              <GroupGamesCard/>
+            </div>
+        </div>
+       <div class="col mx-3 bg-grey">
+          <div class="row bg-c4">
+            <div class="col d-flex justify-content-center pt-2"><h2>Wislist</h2></div>
+            </div>
+            <div class="cardholder">
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+               <GroupGamesCard/>
+            </div>
+        </div> <div class="col mx-3 bg-grey">
+          <div class="row bg-c6">
+            <div class="col d-flex justify-content-center pt-2"><h2>Groups</h2></div>
+            </div>
+            <div class="cardholder">
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+              <GroupGamesCard/>
+               <GroupGamesCard/>
+                <GroupGamesCard/>
+                 <GroupGamesCard/>
+                <GroupGamesCard/>
+            </div>
+        </div>
+    </div>
+
+
   </div>
 </template>
 
 
 <script>
+import GroupGamesCard from "../components/GroupGamesCard.vue";
 export default {
-  setup(){
-    return {}
-  }
+    setup() {
+        return {};
+    },
+    components: { GroupGamesCard }
 }
 </script>
 
 
 <style lang="scss" scoped>
 .banner{
-  min-height: 200px;
+  min-height: 188px;
   background-size: cover;
   background-position: center center;
   background-image: url(https://images.unsplash.com/photo-1610890716171-6b1bb98ffd09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80);
@@ -54,6 +111,8 @@ export default {
 }
 
 .cardholder{
-  min-height: 85vh;
+  max-height: 44vh;
+  overflow-y: auto;
+  overflow-x: hidden;
 }
 </style>
