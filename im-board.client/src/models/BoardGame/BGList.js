80f3c37d96afc47f317cc1fac02406abb1794aa3
molly-nettleton <mollyelainen@gmail.com>
2022-10-14 10:37:48 -0600

    more model shit - molly

--- im-board.client/src/models/BoardGame/BGList.js
+++ im-board.client/src/models/BoardGame/BGList.js
@@ -2,11 +2,11 @@ import { Account } from "../Account.js"
 
 export class BGList {
   constructor(data) {
-    this.id = data.id
+    this.listId = data.id
     this.boardGameName = data.name 
     this.account = new Account(data.account)
     this.accountId = data.accountId
-    this.createdAt = data.createdAt
+    this.addedToListAt = data.createdAt
     this.gameId = data.gameId
     this.rating = data.rating
     this.listName = data.type
