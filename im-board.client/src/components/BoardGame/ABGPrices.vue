0b232e6724f5ab177e9d863b8d6d4cc862c348c8
molly-nettleton <mollyelainen@gmail.com>
2022-10-13 22:28:13 -0600

    price card update - molly

--- im-board.client/src/components/BoardGame/ABGPrices.vue
+++ im-board.client/src/components/BoardGame/ABGPrices.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="component justify-content-center price-info  my-2 text-center p-3 elevation-1 "
+  <div class="component justify-content-center bg-light price-info my-2 text-center p-3 elevation-1"
     v-if="price.in_stock == true">
     <h2>{{price.store_name}}</h2>
-    <h5>{{price.name}}</h5>
     <div class="d-flex text-center justify-content-center gap-3">
-      <p>{{price.updated_at_ago}}</p>
-      <b>{{price.price_text}}</b>
+       <p><b>Price:</b> {{price.price_text}}</p>
+       <div></div>
+       <p><b>Updated:</b> {{price.updated_at_ago}}</p>
+     
     </div>
 
-    <a :href="price.url" target="_blank">Buy here!</a>
+    <a class="btn bg-c6 elevation-5" :href="price.url" target="_blank">Buy here!</a>
   </div>
 </template>
 
