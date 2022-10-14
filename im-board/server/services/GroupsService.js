77532478f9a7567a00a469357c34322f9bbe53f0
daniel le <daniel97.le@gmail.com>
2022-10-13 19:21:32 -0600

    hello

--- im-board/server/services/GroupsService.js
+++ im-board/server/services/GroupsService.js
@@ -3,7 +3,9 @@ import { BadRequest, Forbidden } from "../utils/Errors.js";
 import { groupMembersService } from "./GroupMembersService.js";
 class GroupsService {
   async getMyGroups(accountId) {
-    const groups = await dbContext.GroupMembers.find({ accountId })
+    const groups = await dbContext.GroupMembers.find({ accountId }).populate(
+      "group"
+    );
     if (!groups) {
       throw new BadRequest("bad or invalid accountId");
     }
