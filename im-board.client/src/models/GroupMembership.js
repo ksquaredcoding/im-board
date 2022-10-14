e29e237448f9c3f38a3b28c265d47a380dce0eb3
TungLe0319 <tung.le0319@gmail.com>
2022-10-14 16:29:56 -0600

    tung

--- im-board.client/src/models/GroupMembership.js
+++ im-board.client/src/models/GroupMembership.js
@@ -5,11 +5,10 @@ export class GroupMemberShip {
   constructor(data) {
     this.id = data.id;
     this.createdAt = data.createdAt;
-this.group = new Group(data.group)
-    this.creatorId = data.creatorId;
-    this.coverImg = data.coverImg;
-    this.name = data.name;
-    this.groupMemberIds = data.groupMemberIds;
+// this.group = new Group(data.group)
+
+this.group = data.group
+ 
     this.groupId = data.groupId
   }
 }
