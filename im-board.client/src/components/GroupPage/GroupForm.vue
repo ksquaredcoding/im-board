0cfdf62838ab542257600feabc93ae79a08b45a9
molly-nettleton <mollyelainen@gmail.com>
2022-10-15 16:12:47 -0600

    groupform - molly

--- im-board.client/src/components/GroupPage/GroupForm.vue
+++ im-board.client/src/components/GroupPage/GroupForm.vue
@@ -1,41 +1,33 @@
 <template>
-  <div
-    class="modal"
-    id="groupForm"
-    tabindex="-1"
-    aria-labelledby="groupFormLabel"
-    aria-hidden="true"
->
+  <div class="modal" id="groupForm" tabindex="-1" aria-labelledby="groupFormLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content bg-dark">
         <div class="modal-body FORM">
           <!-- -------------------SECTION FORM----------------------------------- -->
           <form @submit.prevent="handleSubmit" class="">
             <div class="row">
-              <div class="col-md-6">
-                <div>
-                  <img
-                    :src="editable.coverImg"
-                    alt=""
-                    class="forcedImg smallerImg mt-2 Img1"
-                  />
+              <div class="col-md-6 ">
+                <div class="">
+                  <img :src="editable.coverImg" alt="" class="forcedImg smallerImg mt-2 Img1 bg-light opacity-25" />
                 </div>
                 <div class="mt-3 inputBox">
-                  <input type="url" class=" " v-model="editable.coverImg" required aria-required="true"  />
-                  <span>CoverImg</span>
+                  <input type="url" class="bg-light opacity-50 " v-model="editable.coverImg" required
+                    aria-required="true" />
+                  <span class="font">CoverImg</span>
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="mt-3 inputBox">
-                  <input type="text" class=" " v-model="editable.name"  aria-required="true" required/>
-                  <span>group Name</span>
+                  <input type="text" class="bg-light opacity-50" v-model="editable.name" aria-required="true"
+                    required />
+                  <span class="font">Group Name</span>
                 </div>
               </div>
             </div>
 
             <div class="my-3">
-              <button class="btn btn-success selectable" type="submit" data-bs-dismiss="modal" >
-                CreateGroup
+              <button class="btn btn-success selectable font" type="submit" data-bs-dismiss="modal">
+                Create Group
               </button>
             </div>
           </form>
@@ -43,11 +35,11 @@
       </div>
     </div>
 
-  
+
   </div>
 
 
-<!-- 
+  <!-- 
   <div
  
     class="modal"
@@ -122,7 +114,7 @@ export default {
         }
       },
 
-      
+
       async handleSubmitForEdit() {
         try {
           await groupsService.editGroup(editable.value);
@@ -139,8 +131,9 @@ export default {
 <style scoped>
 .forcedImg {
   border-radius: 4px;
-  border: dashed 4px rgb(146, 197, 245);
+  border: dashed 4px #ff5e00;
 }
+
 .checkbox {
   width: 30px;
   height: 30px;
@@ -161,6 +154,7 @@ export default {
 .Img1 {
   transition: all 0.5s ease;
 }
+
 .Img1:hover {
   transform: scale(1.01);
   transition: all 0.4s ease;
@@ -173,27 +167,30 @@ export default {
   padding: 0 10px;
   font-size: 0.65em;
 
-  border: 1px solid #92c5f5;
+  border: 1px solid #ff5e00;
 
   letter-spacing: 0.2em;
   transition: all 1s ease;
 }
+
 .inputBox {
   position: relative;
   width: 250px;
 }
+
 /* ------------------------------------------------------------------------------------ */
 .inputBox2 {
   position: relative;
   width: auto;
 }
+
 .inputBox2 textarea {
   width: 100%;
 
   text-align: right;
   padding: 10px;
-  border: 1px solid #92c5f5;
-  border-bottom: 3px solid #c4def7;
+  border: 1px solid #ffbb00;
+  border-bottom: 3px solid #ff5e00;
   border-radius: 5px;
   outline: none;
   color: #fff;
@@ -202,6 +199,7 @@ export default {
   transition: all 1.5s ease;
   box-shadow: 0.25px 0.25px 10px rgba(243, 236, 236, 0.308);
 }
+
 .inputBox2 span {
   position: absolute;
   left: 0;
@@ -213,17 +211,18 @@ export default {
   color: #f1ecf180;
   transition: all 1s ease;
 }
-.inputBox2 textarea:valid ~ span,
-.inputBox2 textarea:focus ~ span {
+
+.inputBox2 textarea:valid~span,
+.inputBox2 textarea:focus~span {
   color: #27132a;
   transform: translateX(10px) translateY(-70px);
   padding: 0 10px;
   font-size: 1em;
   border-radius: 4px;
   font-weight: bold;
-  background: #92c5f5;
-  border-left: 1px solid #92c5f5;
-  border-right: 1px solid #92c5f5;
+  background: #ffbb00;
+  border-left: 1px solid #ff5e00;
+  border-right: 1px solid #ff5e00;
   letter-spacing: 0.2em;
   transition: all 1s ease;
 }
@@ -232,8 +231,8 @@ export default {
 .inputBox input {
   width: 100%;
   padding: 10px;
-  border: 1px solid #92c5f5;
-  border-bottom: 3px solid #c4def7;
+  border: 1px solid #ffbb00;
+  border-bottom: 3px solid #ff5e00;
   border-radius: 5px;
   outline: none;
   color: #fff;
@@ -242,6 +241,7 @@ export default {
   transition: all 1.5s ease;
   box-shadow: 0.25px 0.25px 10px rgba(243, 236, 236, 0.308);
 }
+
 .inputBox span {
   position: absolute;
   left: 0;
@@ -253,17 +253,18 @@ export default {
   color: #f1ecf180;
   transition: all 1s ease;
 }
-.inputBox input:valid ~ span,
-.inputBox input:focus ~ span {
+
+.inputBox input:valid~span,
+.inputBox input:focus~span {
   color: #27132a;
   transform: translateX(10px) translateY(-7px);
   padding: 0 10px;
   font-size: 0.65em;
   border-radius: 4px;
   font-weight: bold;
-  background: #92c5f5;
-  border-left: 1px solid #92c5f5;
-  border-right: 1px solid #92c5f5;
+  background: #ffbb00;
+  border-left: 1px solid #ff5e00;
+  border-right: 1px solid #ff5e00;
   letter-spacing: 0.2em;
   transition: all 1s ease;
 }
@@ -275,9 +276,13 @@ export default {
   border-radius: 2px;
 } */
 .modal-body {
-  background-image: url(https://www.pixelstalk.net/wp-content/uploads/images6/21-9-HD-Wallpaper-Free-download.jpg);
+  background-image: url(https://images.unsplash.com/photo-1615775642101-402e4d2c0e0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80);
   background-size: cover;
   background-position: center;
   box-shadow: 1px 1px 10px rgba(243, 236, 236, 0.308);
+}
+
+.font {
+  font-family: 'Baloo 2', cursive;
 }
 </style>
