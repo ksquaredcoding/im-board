b0ed86330e4043c9c65deded737855d738b517c5
molly-nettleton <mollyelainen@gmail.com>
2022-10-15 16:27:21 -0600

    searbar. molly

--- im-board.client/src/components/BoardGame/Searchbar.vue
+++ im-board.client/src/components/BoardGame/Searchbar.vue
@@ -1,12 +1,14 @@
 <template>
- 
-    <form @submit.prevent="getBoardGamesByQuery()">
-      <div class=" d-flex">
-        <input  v-model="editable" type="text" class="form-control" placeholder="SkullKing,Catan,Twilight Imperium, etc.. " aria-label="Search" aria-describedby="button-addon2">
-<button class="btn button-50 py-1 px-2 mx-2" type="submit" id="button-addon2"><i class="mdi mdi-magnify mx-2" alt="" title="search By Name" ></i></button>
-      </div>
 
-    </form>
+  <form @submit.prevent="getBoardGamesByQuery()">
+    <div class=" d-flex">
+      <input v-model="editable" type="text" class="form-control phtext" placeholder="Search for board game . . ."
+        aria-label="Search" aria-describedby="button-addon2">
+      <button class="btn button-50 py-1 px-2 mx-2" type="submit" id="button-addon2"><i class="mdi mdi-magnify mx-2"
+          alt="" title="search By Name"></i></button>
+    </div>
+
+  </form>
 
 
 </template>
@@ -18,17 +20,17 @@ import { atlasGamesService } from "../../services/AtlasGamesService.js";
 import Pop from "../../utils/Pop.js";
 
 export default {
-  setup(){
-const editable = ref('')
+  setup() {
+    const editable = ref('')
 
     return {
-editable,
-      async getBoardGamesByQuery(){
-try {
-    await atlasGamesService.getBoardGamesByQuery(editable.value)
-  } catch (error) {
- 
-  }
+      editable,
+      async getBoardGamesByQuery() {
+        try {
+          await atlasGamesService.getBoardGamesByQuery(editable.value)
+        } catch (error) {
+
+        }
       }
     }
   }
@@ -37,5 +39,7 @@ try {
 
 
 <style lang="scss" scoped>
-
+.phtext {
+  font-size: 12px;
+}
 </style>
