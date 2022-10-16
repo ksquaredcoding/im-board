f2b9ab216dc3932a783abd7b3f4150b83e20da68
daniel le <daniel97.le@gmail.com>
2022-10-16 14:47:05 -0600

    commit

--- im-board.client/src/models/Account.js
+++ im-board.client/src/models/Account.js
@@ -1,13 +1,12 @@
 export class Account {
   constructor(data) {
-    this.id = data.id
-    this.email = data.email
-    this.name = data.name
-    this.picture = data.picture
+    this.id = data.id;
+    this.email = data.email;
+    this.name = data.name;
+    this.picture = data.picture;
     // TODO add additional properties if needed
 
-this.coverImg= data.coverImg 
-this.bio=data.bio 
-
+    this.coverImg = data.coverImg;
+    this.bio = data.bio;
   }
 }
