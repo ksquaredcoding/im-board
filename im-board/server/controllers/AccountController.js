6f2780e35e65c5f703457eb0edacb825afc99e8b
daniel le <daniel97.le@gmail.com>
2022-10-16 14:28:57 -0600

    profilesController

--- im-board/server/controllers/AccountController.js
+++ im-board/server/controllers/AccountController.js
@@ -9,9 +9,6 @@ export class AccountController extends BaseController {
   constructor() {
     super("/account");
     this.router
-    .get("/:accountId", this.getUserProfile)
-    .get("/boardgames/:id", this.getProfileBoardGames)
-    .get("/groups/:id", this.getProfileGroups)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get("", this.getUserAccount)
       .get("/boardgames", this.getAccountLists)
@@ -57,24 +54,7 @@ export class AccountController extends BaseController {
   }
 
   // TODO account boardGame
-  async getProfileBoardGames(req, res, next) {
-    try {
-      const boardGames = await boardGamesService.getBoardGamesByAccountId(
-        req.params.id
-      );
-      res.send(boardGames);
-    } catch (error) {
-      next(error);
-    }
-  }
-  async getProfileGroups(req, res, next) {
-    try {
-      const groups = await groupsService.getMyGroups(req.params.id);
-      res.send(groups);
-    } catch (error) {
-      next(error);
-    }
-  }
+ 
   async editMyAccount(req, res, next) {
     try {
       const account = await accountService.updateAccount(
--- im-board/server/controllers/ProfilesController.js
+++ im-board/server/controllers/ProfilesController.js
@@ -1,29 +1,54 @@
+import { boardGamesService } from '../services/BoardGamesService.js';
+import { groupsService } from '../services/GroupsService.js';
 import { profileService } from '../services/ProfileService.js'
 import BaseController from '../utils/BaseController'
 
 export class ProfilesController extends BaseController {
   constructor() {
-    super('api/profiles')
+    super("api/profiles");
     this.router
-      .get('', this.getProfiles)
-      .get('/:id', this.getProfile)
+      .get("", this.getProfiles)
+      .get("/:id", this.getProfile)
+      .get("/boardgames/:id", this.getProfileBoardGames)
+      .get("/groups/:id", this.getProfileGroups);
   }
 
   async getProfiles(req, res, next) {
     try {
-      const profiles = await profileService.findProfiles(req.query.name, req.query.offset)
-      res.send(profiles)
+      const profiles = await profileService.findProfiles(
+        req.query.name,
+        req.query.offset
+      );
+      res.send(profiles);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   async getProfile(req, res, next) {
     try {
-      const profile = await profileService.getProfileById(req.params.id)
-      res.send(profile)
+      const profile = await profileService.getProfileById(req.params.id);
+      res.send(profile);
     } catch (error) {
-      next(error)
+      next(error);
+    }
+  }
+  async getProfileBoardGames(req, res, next) {
+    try {
+      const boardGames = await boardGamesService.getBoardGamesByAccountId(
+        req.params.id
+      );
+      res.send(boardGames);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getProfileGroups(req, res, next) {
+    try {
+      const groups = await groupsService.getMyGroups(req.params.id);
+      res.send(groups);
+    } catch (error) {
+      next(error);
     }
   }
 }
