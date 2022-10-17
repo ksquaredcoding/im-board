9174bbc6dfd14b7a5b312fc19d0800aea18a642c
TungLe0319 <tung.le0319@gmail.com>
2022-10-17 16:22:13 -0600

    tung

--- im-board.client/src/components/GroupPage/GameNightCard.vue
+++ im-board.client/src/components/GroupPage/GameNightCard.vue
@@ -55,12 +55,20 @@
               <h5 class="mt-1">Attending:</h5>
             </div>
             <div class="p-2 bg-c2 text-center" v-if="gamenight?.groupMemberIds">
-              <router-link
+             
+
+
+<TransitionGroup
+          name="custom-classes"
+          enterActiveClass="animate__zoomIn animate__animated"
+          leaveActiveClass="animate__zoomOut animate__animated"
+        >
+         <router-link
                 :to="{ name: 'Profile', params: { id: g.id } }"
                 v-for="g in gamenight?.groupMemberIds"
                 :key="g.id"
               >
-                <img
+     <img
                   :src="g.picture"
                   :alt="g.name"
                   :title="g.name"
@@ -68,7 +76,16 @@
                   width="45"
                   class="rounded-circle box-shadow m-1 profile-img"
                 />
-              </router-link>
+
+
+              
+   </router-link>
+        </TransitionGroup>
+
+
+
+           
+           
               <!-- <img :src="g.picture" :alt="g.name" height="55" class="rounded-circle me-1 box-shadow"
                 v-for="g in gamenight?.groupMemberIds" :groupMemberId="g" /> -->
             </div>
@@ -176,5 +193,21 @@ export default {
 
 .noborder {
   border: none;
+}
+
+
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>
