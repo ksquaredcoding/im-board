0752aad19594e4544495ce8cbd41c9a26f2678cb
TungLe0319 <tung.le0319@gmail.com>
2022-10-17 11:23:51 -0600

    tung

--- im-board.client/src/components/BoardGame/ABGPrices.vue
+++ im-board.client/src/components/BoardGame/ABGPrices.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="component justify-content-center bg-light price-info my-2 text-center p-3 elevation-1 rounded-2"
-    v-if="price.in_stock == true">
+  >
     <h2 class="font">{{price.store_name}}</h2> 
     <h4>{{price.name}}</h4>
     <div class="d-flex text-center justify-content-center gap-3">
-      <p><b>Price:</b> {{price.price_text}}</p>
+      <p><b >Price:</b> {{price.price_text}}</p>
      
       <p><b>Updated:</b> {{price.updated_at_ago}}</p>
     </div>
-    <a class="btn button-51 elevation-5" :href="price.url" target="_blank">Buy here!</a>
+    <a  class="btn button-51 elevation-5" :href="price.url" target="_blank">Buy here!</a>
   </div>
 </template>
 
