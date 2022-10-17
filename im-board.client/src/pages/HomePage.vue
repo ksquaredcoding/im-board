cbecb9e31abe326a1539b7fb8e933af649e7c16c
TungLe0319 <tung.le0319@gmail.com>
2022-10-17 15:06:45 -0600

    tung

--- im-board.client/src/pages/HomePage.vue
+++ im-board.client/src/pages/HomePage.vue
@@ -125,8 +125,8 @@ export default {
     }
 
     onMounted(() => {
-getBoardGamesByPopularity()
- getBoardGamesByDiscount()
+// getBoardGamesByPopularity()
+//  getBoardGamesByDiscount()
       // getBoardGamesByPrice()
       getForumPosts();
       // AppState.skip = 0;
