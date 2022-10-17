3bd112d558cbeb6c628722518f4474442489fc48
TungLe0319 <tung.le0319@gmail.com>
2022-10-17 11:19:01 -0600

    tung

--- im-board.client/src/components/Footer.vue
+++ im-board.client/src/components/Footer.vue
@@ -1,42 +1,45 @@
 <template>
-<div class="container">
+<div class="container ">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+    <p class="col-md-4 mb-0 text-muted footer-text fs-5">© 2022 I'm Game, Inc</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
       <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
     </a>
 
     <ul class="nav col-md-4 justify-content-end">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-      <li class="nav-item"><a href="topOfSearchPage" class="nav-link px-2 text-muted">Back To The Top</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted footer-text">Home</a></li>
+      <li class="nav-item"><a href="#/search" class="nav-link px-2 text-muted footer-text">Search</a></li>
+
+      <li class="nav-item"><button type="button" class="btn footer-text text-muted" data-bs-toggle="modal" data-bs-target="#FAQ">
+  FAQ
+</button>
+</li>
+      <li class="nav-item"><a href="#/about" class="nav-link px-2 text-muted footer-text">About</a></li>
     </ul>
   </footer>
 </div>
+<FAQModal/>
 </template>
 
 <script>
 import { computed } from "@vue/reactivity";
 import { useRoute, useRouter } from "vue-router";
 import { AppState } from "../AppState.js";
+import FAQModal from "./FAQModal.vue";
 
 export default {
-  props: {
-
-  },
-
-  setup(props) {
-const route = useRoute()
-const router = useRouter()
-    return {
-      route,
-      router,
-groupPage: computed(()=> AppState.activeGroup)
-    };
-  },
+    props: {},
+    setup(props) {
+        const route = useRoute();
+        const router = useRouter();
+        return {
+            route,
+            router,
+            groupPage: computed(() => AppState.activeGroup)
+        };
+    },
+    components: { FAQModal }
 };
 </script>
 
@@ -53,6 +56,18 @@ groupPage: computed(()=> AppState.activeGroup)
   height: 300px;
   width: 300px;
   object-fit: cover;
+}
+
+.footer-text {
+  background: linear-gradient(to right,
+      #ffbb00,
+      #ff5e00);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  text-align: center; 
+  font-size: 30px;
+  font-weight: bolder;
+  font-family: 'Baloo 2', cursive;
 }
 
 
