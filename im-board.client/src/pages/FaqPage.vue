b52febb7a818365d7edd82455dea856464dab9d0
molly-nettleton <mollyelainen@gmail.com>
2022-10-20 16:43:11 -0600

    stuff

--- im-board.client/src/pages/FaqPage.vue
+++ im-board.client/src/pages/FaqPage.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="container-fluid">
-    <div class="d-flex justify-content-center">
-      <h1>Site Tour</h1>
-    </div>
+    <div class="d-flex justify-content-center mt-2">
+      <div class="col-md-5 bg-c6 text-center rounded-5"> <h1>Site Tour</h1>
+      </div>
+    </div> 
+<div class="row">
+  <div class="col-md-12 d-flex justify-content-center">
+    <img src="src\assets\img\accountpage.png" alt="" class="img-fluid"></div>
+    <div class="col-md-12 d-flex justify-content-center mt-5">
+    <img src="" alt="" class="img-fluid"></div>
+  </div>
   </div>
 </template>
 
@@ -15,5 +22,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 </style>
