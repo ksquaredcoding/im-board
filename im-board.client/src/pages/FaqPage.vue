e9ad58f84c74fce7aeb3d09137f7ab7103eca12d
molly-nettleton <mollyelainen@gmail.com>
2022-10-20 15:48:12 -0600

    hi

---
+++ im-board.client/src/pages/FaqPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid">
+    <div class="d-flex justify-content-center">
+      <h1>Site Tour</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
