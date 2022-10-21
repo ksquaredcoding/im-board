adfc3ea5be5f4e0e28b3ef47d39dbd8abd553af8
daniel le <daniel97.le@gmail.com>
2022-10-20 19:47:01 -0600

    reset sockets

--- im-board.client/src/main.js
+++ im-board.client/src/main.js
@@ -1,14 +1,13 @@
-import '@mdi/font/css/materialdesignicons.css';
-import 'bootstrap';
-import { createApp } from 'vue';
+import "@mdi/font/css/materialdesignicons.css";
+import "bootstrap";
+import { createApp } from "vue";
 // @ts-ignore
-import App from './App.vue';
-import './handlers/MemberHandler.js';
+import App from "./App.vue";
 
-import { registerGlobalComponents } from './registerGlobalComponents';
-import { router } from './router';
+import { registerGlobalComponents } from "./registerGlobalComponents";
+import { router } from "./router";
 
 const root = createApp(App);
 registerGlobalComponents(root);
 
-root.use(router).mount('#app');
+root.use(router).mount("#app");
