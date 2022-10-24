ab32c9b6a04f33c71a9ade507269cd1c55cd7ff8
TungLe0319 <tung.le0319@gmail.com>
2022-10-24 10:02:34 -0600

    tung

---
+++ im-board.client/src/components/ProfileImage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="example"></div>
+</template>
+
+<script>
+import { Account } from "../models/Account.js";
+
+export default {
+  props: {
+profile: {type: Account, required:true}
+  },
+
+  setup(props) {
+
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.text-shadow{
+  color: aliceblue;
+  text-shadow: 1px 1px black, 0px 0px 5px salmon;
+  font-weight: bold;
+  letter-spacing: 0.08rem
+  /* Second Color  in text-shadow is the blur */
+}
+.forcedImg{
+  height: 300px;
+  width: 300px;
+  object-fit: cover;
+}
+
+
+</style>
