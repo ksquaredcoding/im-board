958e68ae470815a0d43fd422ee9a81f4b53087cc
ksquaredcoding <ksquaredcoding@gmail.com>
2022-12-19 09:47:13 -0700

    attempt at launch

---
+++ im-board/client/assets/SiteTourPage.d63ecf23.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./index.22bc8e8b.js";const s={setup(){return{}}},c={class:"component"};function n(r,a,_,p,i,u){return o(),t("div",c)}const f=e(s,[["render",n]]);export{f as default};
