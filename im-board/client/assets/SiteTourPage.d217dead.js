1de4191a29afe2c113f2607b9425005df3b58fc1
Tung N Le <tung.le0319@gmail.com>
2023-01-19 23:45:52 -0700

    test-21

--- im-board/client/assets/SiteTourPage.d217dead.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o}from"./index.603166ed.js";const s={setup(){return{}}},c={class:"component"};function n(r,a,_,p,i,u){return o(),t("div",c)}const f=e(s,[["render",n]]);export{f as default};
